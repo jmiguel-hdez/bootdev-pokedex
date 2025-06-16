51c3afa1d4cfc521ce590a7640036414add155ae
jmiguel-hdez <jmiguel.hdez@gmail.com>
2025-06-15 19:14:24 -0500

    solution to lsn04 repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,25 @@
+import { createInterface } from "node:readline";
+
 export function cleanInput(input: string): string[] {
   return input
     .toLowerCase()
     .trim()
     .split(' ')
     .filter((word) => word !== "");
+}
+
+export function startREPL(): void {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex >"
+  });
+  rl.prompt();
+  rl.on("line", (line: string) => {
+    const words = cleanInput(line);
+    if (words.length !== 0) {
+      console.log(`Your command was: ${words[0]}`)
+    }
+    rl.prompt();
+  });
 }
